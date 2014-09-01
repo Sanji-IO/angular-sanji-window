47d025e72497de0419e177e5c918c2d16f7c1ad9
zackyang <zack9433@gmail.com>
2014-09-01 12:41:18 +0800

    Modify

--- src/window/sanji-window.js
+++ src/window/sanji-window.js
@@ -1,7 +1,7 @@
 ;(function() { 'use strict';
 
   angular
-    .module('sanji.window', ['google-maps', 'pasvaz.bindonce', 'sanji.validator'])
+    .module('sanji.window', ['google-maps', 'pasvaz.bindonce', 'sanji.validator', 'ngAnimate'])
     .constant('_', window._)
     .provider('sanjiWindowConfig', SanjiWindowConfig)
     .directive('mxHide', sanjiHide)
