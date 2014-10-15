e106373ca5fd17f116f3ecfd350b62d8c2c59d1d
zackyang <zack9433@gmail.com>
2014-10-15 15:50:32 +0800

    build dist

--- dist/sanji-window.js
+++ dist/sanji-window.js
@@ -1,7 +1,7 @@
 ;(function() { 'use strict';
 
   angular
-    .module('sanji.window', ['google-maps', 'pasvaz.bindonce', 'sanji.validator', 'ngAnimate', 'angularFileUpload'])
+    .module('sanji.window', ['pasvaz.bindonce', 'sanji.validator', 'ngAnimate', 'angularFileUpload'])
     .constant('_', window._)
     .provider('SanjiWindowConfig', SanjiWindowConfig)
     .directive('sanjiHideWindow', sanjiHideWindow)
