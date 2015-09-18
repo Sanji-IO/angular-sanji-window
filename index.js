6163cd2514356ee2475221503eb162d9c6bf4dce
zackyang <zack9433@gmail.com>
2015-09-18 17:56:58 +0800

    Modify dependency

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+import 'angular-material.css';
+import 'angular-material-icons.css';
+
+import 'svg-morpheus';
 import 'angular';
+import 'angular-material';
 import sanjiWindow from './app/sanji-window';
 
 export default sanjiWindow
